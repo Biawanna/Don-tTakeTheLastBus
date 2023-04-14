9835b963d3d776c40c8c2ea16e174e458e6febcd
Biawanna <ash.biawanna@gmail.com>
2023-04-13 18:35:26 -0700

    Created player pause script. Moved pause button press methods to pause script.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,5 @@
 using Autohand;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -10,8 +9,6 @@ public class Player : MonoBehaviour
     [SerializeField] private LineRenderer leftGrabPointer;
 
     private AutoHandPlayer autoHandPlayer;
-    private UICanvasController canvasController;
-    private GameManager gameManager;
     public AutoHandPlayer AutoHandPlayer { get { return autoHandPlayer; } }
 
     private void Awake()
@@ -21,21 +18,8 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        canvasController = UICanvasController.Instance;
-        gameManager = GameManager.instance;
 
         leftGrabPointer.enabled = false;
         rightGrabPointer.enabled = false;
-    }
-
-    private void Update()
-    {
-        if (SceneManager.GetActiveScene().buildIndex != 0 && !gameManager.GameOver)
-        {
-            if (Input.GetButtonDown("XRI_Right_PrimaryButton") || Input.GetButtonDown("XRI_Left_PrimaryButton"))
-            {
-                canvasController.Pause();
-            }
-        }
     }
 }
